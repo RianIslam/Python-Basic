6829485a9b52fc188aac3a6580467192ceeeac77
RianIslam <rianislamdr@gmail.com>
2021-02-23 21:56:32 +0600

    basic python

---
+++ again.py
@@ -0,0 +1,90 @@
+#basic of python
+# def add_two_values(first,second):
+#     return first + second
+#
+# number_1 = 30
+# number_2 = 84
+# sum = add_two_values(number_1, number_2)
+# print(f"{number_1} + {number_2} = {sum}")
+#
+#
+# username = input('your name :')
+# print(f'hello {username}')
+#
+#
+#
+# print('hello wold');
+# print('abc')
+
+
+
+
+
+
+
+
+
+
+
+
+
+#basic operation of Python
+
+# number_value = 44
+# second_value =3
+#
+# addition = number_value+second_value
+# subtruction = number_value- second_value
+# mul = number_value * 4
+# div = number_value / 3
+# div_int =  number_value//3
+#
+# print(f"mul: {mul}")
+# print(f"div:{div}")
+# print(f"div_int: {div_int}")
+# print(f"Additon: {addition}")
+# print(f"Subtruction : {subtruction}")
+# value_name=6
+# name = "Rian"
+# value_with_name = name+" "+ str(value_name)
+#
+# print(value_with_name)
+# print(f"length of the word: {len(value_with_name)}")
+
+
+
+
+
+
+
+
+#list operation of python
+# grocery_list = ["rice","potato","tomato"]
+# print(grocery_list)
+# grocery_list.append("new")
+# print(grocery_list)
+# l2 =list()
+# print(l2)
+# l2.append(3)
+# print(l2)
+#
+#
+# secont_item = grocery_list[3]
+# print(secont_item)
+# print(grocery_list[-3])
+# grocery_list.sort()
+# print(grocery_list)
+# grocety_number = [6,564,6674,45,6,7,8,9,56]
+# grocety_number.sort()
+# print(grocety_number)
+
+
+#Condition in pyhon
+
+your_marks = input('what is your marks in programmin')
+def show_grade(grade):
+    print(f"You got :")
+if your_marks>80:
+    print('you got A+')
+
+elif your_marks <
