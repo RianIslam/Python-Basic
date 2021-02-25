90d3d7f4b8cd92f701d39b4cf049a8f6bf935434
RianIslam <rianislamdr@gmail.com>
2021-02-25 19:10:57 +0600

    class in python

--- OOP_ONE.py
+++ OOP_ONE.py
@@ -2,20 +2,22 @@ def method_name(a,b):
     print("A method")
 
 class Person:
-    def __init__(self,person_name):
+    def __init__(self,person_name,date_of_birth,ht):
         self.name =person_name
+        self.date_of_birth = date_of_birth
+        self.height = ht
+
 
 
     def get_name(self):
         return self.name
 
-
+    def get_summary(self):
+        return f"name: {self.name}, DOB: {self.date_of_birth}, Height: {self.height}"
 
 method_name(23,45)
 
-a_person = Person("Rian Islam")
-b_person = Person("Islam")
+a_person = Person("Rian Islam", "1009", "6 feet")
 
 
-print(a_person.get_name())
-print(b_person.get_name())
+print(a_person.get_summary())
